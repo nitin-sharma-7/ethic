b99f880000c70644cb22286383978a2cd0cf1e7e
nitin-sharma-7 <ns17740654@gmail.com>
2025-11-13 15:16:48 +0530

    fav

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,10 +7,20 @@ import HeroDupe from "@/components/HeroDupe";
 import ConsultationForm from "@/components/ConsultationForm";
 import SolutionsTabs from "@/components/SolutionsTabs";
 import Testimonials from "@/components/Testimonials";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ethice Capital</title>
+        <meta
+          name="description"
+          content="Ethice Capital is a pioneering investment firm dedicated to ethical and sustainable investing. We focus on creating positive social and environmental impact while delivering strong financial returns for our clients."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <HeroSection />
       <WhatWeDo />
       <HeroDupe />
